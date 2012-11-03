233c239405d634cf11db54d078e275025fd677f1
fuad <fuad_assayadi@yahoo.com>
2012-11-04 00:49:33 +0200

    Create a rails application a

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookapp::Application.config.secret_token = '4f3589d927ca1b6cc4094bb20f596279d552187eaed8cff264a3a216412b63cfba2de66d576d77e2f9854449235a1644c6f2c9403d7dc7ed9dee2edbfc4d7ca7'
